2eea82a86cb8ab3696a56e52719086a745707bb1
STJr2020 <stephentjr97@gmail.com>
2020-05-06 16:27:46 -0400

    Project

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  linkedIN
+//
+//  Created by Edward Dillon on 5/5/20.
+//  Copyright © 2020 Edward Dillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
